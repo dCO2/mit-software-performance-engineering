b45c6cdd5578a05a6ab216e0418904a29241b929
Caleb Durojaiye <dco2.caleb@gmail.com>
2021-12-18 17:29:18 +0100

    Initial Optimization:
    - Removed bitarray_rotate_left_one
    - Implemented bitarray_reverse
    - Rewrote bitarray_rotate_left that uses bitarray_reverse
    - Rewrote bitarray_rotate that uses bitarray_rotate_left

--- project-01-bit-hacks/bitarray.c
+++ project-01-bit-hacks/bitarray.c
@@ -73,9 +73,9 @@ static void bitarray_rotate_left(bitarray_t* const bitarray,
 // The subarray spans the half-open interval
 // [bit_offset, bit_offset + bit_length)
 // That is, the start is inclusive, but the end is exclusive.
-static void bitarray_rotate_left_one(bitarray_t* const bitarray,
-                                     const size_t bit_offset,
-                                     const size_t bit_length);
+// static void bitarray_rotate_left_one(bitarray_t* const bitarray,
+//                                      const size_t bit_offset,
+//                                      const size_t bit_length);
 
 // Portable modulo operation that supports negative dividends.
 //
@@ -105,8 +105,11 @@ static size_t modulo(const ssize_t n, const size_t m);
 // however, so as long as you always use bitarray_get and bitarray_set
 // to access bits in your bitarray, this reverse representation should
 // not matter.
-static char bitmask(const size_t bit_index);
+//static char bitmask(const size_t bit_index);
 
+static void bitarray_reverse(bitarray_t* const bitarray,
+                                     int begin,
+                                     int end);
 
 // ******************************* Functions ********************************
 
@@ -153,7 +156,7 @@ bool bitarray_get(const bitarray_t* const bitarray, const size_t bit_index) {
   // get the byte; we then bitwise-and the byte with an appropriate mask
   // to produce either a zero byte (if the bit was 0) or a nonzero byte
   // (if it wasn't).  Finally, we convert that to a boolean.
-  return (bitarray->buf[bit_index / 8] & bitmask(bit_index)) ?
+  return (bitarray->buf[bit_index / 8] & (1 << (bit_index % 8))) ?
          true : false;
 }
 
@@ -170,8 +173,8 @@ void bitarray_set(bitarray_t* const bitarray,
   // to clear out the bit we're about to set.  We bitwise-or the result
   // with a byte that has either a 1 or a 0 in the correct place.
   bitarray->buf[bit_index / 8] =
-    (bitarray->buf[bit_index / 8] & ~bitmask(bit_index)) |
-    (value ? bitmask(bit_index) : 0);
+    (bitarray->buf[bit_index / 8] & ~(1 << (bit_index % 8))) |
+    (value ? (1 << (bit_index % 8)) : 0);
 }
 
 void bitarray_randfill(bitarray_t* const bitarray){
@@ -179,6 +182,21 @@ void bitarray_randfill(bitarray_t* const bitarray){
   for (int64_t i=0; i<bitarray->bit_sz/32 + 1; i++){
     ptr[i] = rand();
   }
+}
+
+
+static void bitarray_reverse(bitarray_t* const bitarray,
+                                     int begin,
+                                     int end){
+  while(begin < end){
+    bool temp = bitarray_get(bitarray, end);
+    bitarray_set(bitarray, end, bitarray_get(bitarray, begin));
+    bitarray_set(bitarray, begin, temp);
+
+    begin = begin + 1;
+    end = end - 1;
+  }
+
 }
 
 void bitarray_rotate(bitarray_t* const bitarray,
@@ -201,23 +219,50 @@ static void bitarray_rotate_left(bitarray_t* const bitarray,
                                  const size_t bit_offset,
                                  const size_t bit_length,
                                  const size_t bit_left_amount) {
-  for (size_t i = 0; i < bit_left_amount; i++) {
-    bitarray_rotate_left_one(bitarray, bit_offset, bit_length);
-  }
+  
+  bitarray_reverse(bitarray, bit_offset, bit_offset + bit_left_amount - 1);
+  bitarray_reverse(bitarray, bit_offset + bit_left_amount, bit_offset + bit_length - 1);
+  bitarray_reverse(bitarray, bit_offset, bit_offset + bit_length - 1);
+
 }
 
-static void bitarray_rotate_left_one(bitarray_t* const bitarray,
-                                     const size_t bit_offset,
-                                     const size_t bit_length) {
-  // Grab the first bit in the range, shift everything left by one, and
-  // then stick the first bit at the end.
-  const bool first_bit = bitarray_get(bitarray, bit_offset);
-  size_t i;
-  for (i = bit_offset; i + 1 < bit_offset + bit_length; i++) {
-    bitarray_set(bitarray, i, bitarray_get(bitarray, i + 1));
-  }
-  bitarray_set(bitarray, i, first_bit);
-}
+// void bitarray_rotate(bitarray_t* const bitarray,
+//                      const size_t bit_offset,
+//                      const size_t bit_length,
+//                      const ssize_t bit_right_amount) {
+//   assert(bit_offset + bit_length <= bitarray->bit_sz);
+
+//   if (bit_length == 0) {
+//     return;
+//   }
+
+//   // Convert a rotate left or right to a left rotate only, and eliminate
+//   // multiple full rotations.
+//   bitarray_rotate_left(bitarray, bit_offset, bit_length,
+//                        modulo(-bit_right_amount, bit_length));
+// }
+
+// static void bitarray_rotate_left(bitarray_t* const bitarray,
+//                                  const size_t bit_offset,
+//                                  const size_t bit_length,
+//                                  const size_t bit_left_amount) {
+//   for (size_t i = 0; i < bit_left_amount; i++) {
+//     bitarray_rotate_left_one(bitarray, bit_offset, bit_length);
+//   }
+// }
+
+// static void bitarray_rotate_left_one(bitarray_t* const bitarray,
+//                                      const size_t bit_offset,
+//                                      const size_t bit_length) {
+//   // Grab the first bit in the range, shift everything left by one, and
+//   // then stick the first bit at the end.
+//   const bool first_bit = bitarray_get(bitarray, bit_offset);
+//   size_t i;
+//   for (i = bit_offset; i + 1 < bit_offset + bit_length; i++) {
+//     bitarray_set(bitarray, i, bitarray_get(bitarray, i + 1));
+//   }
+//   bitarray_set(bitarray, i, first_bit);
+// }
 
 static size_t modulo(const ssize_t n, const size_t m) {
   const ssize_t signed_m = (ssize_t)m;
@@ -227,7 +272,7 @@ static size_t modulo(const ssize_t n, const size_t m) {
   return (size_t)result;
 }
 
-static char bitmask(const size_t bit_index) {
-  return 1 << (bit_index % 8);
-}
+// static char bitmask(const size_t bit_index) {
+//   return 1 << (bit_index % 8);
+// }
 
